13c39888c7b9537c8649b4c4e3a2b415debb0698
Shigma <1700011071@pku.edu.cn>
2018-08-14 11:43:33 +0800

    watch framework

--- src/Marklet.ts
+++ src/Marklet.ts
@@ -1,11 +1,36 @@
 import { DocLexer, DocLexerConfig } from './Document'
+import * as path from 'path'
+import * as http from 'http'
+import * as url from 'url'
+import * as fs from 'fs'
+
+const html = fs.readFileSync(path.join(__dirname, '../index.html'), { encoding: 'utf8' })
 
 interface parseOptions {
   input: string
-  output: string
   config: DocLexerConfig
 }
 
 export function parse(options: parseOptions) {
-  
+  return new DocLexer(options.config).parse(options.input)
+}
+
+interface watchOptions {
+  source: string
+  port?: number
+}
+
+export function watch(options: watchOptions) {
+  const port = options.port || 8080
+  http.createServer((request, response) => {
+    const { pathname } = url.parse(request.url)
+    if (pathname === '/') {
+      response.writeHead(200, { 'Content-Type': 'text/html' })
+      response.write(html)
+    } else {
+      response.writeHead(404, { 'Content-Type': 'text/html' })
+    }
+    response.end()
+  }).listen(port)
+  console.log(`Server running at http://localhost:${port}/`)
 }
