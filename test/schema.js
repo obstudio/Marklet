89114716fcfdc6066a369fb256ba0cef3a95c816
jjyyxx <obfishstudio@gmail.com>
2018-08-25 15:25:53 +0800

    validate text

--- test/schema.js
+++ test/schema.js
@@ -1,9 +1,13 @@
+const cheerio = require('cheerio')
 const Ajv = require('ajv')
 
 const validator = new Ajv({
 })
 
-validator.addFormat('text', () => true)
+const textTags = ['br', 'code', 'em', 'strong', 'del', 'span', 'a']
+validator.addFormat('text', (str) =>
+  cheerio.load(str)('body *').toArray().every((element) =>
+    element.type === 'text' || element.type === 'tag' && textTags.includes(element.tagName)))
 
 const validate = validator.compile(require('./schema.json'))
 
