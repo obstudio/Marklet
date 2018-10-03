467af7367cef4b3197d9e5594615f541571e9794
Shigma <1700011071@pku.edu.cn>
2018-10-03 21:42:05 +0800

    basic usage

--- packages/app/comp/app.vue
+++ packages/app/comp/app.vue
@@ -1,12 +1,60 @@
 <script>
 
+const { DocumentLexer } = require('@marklet/parser')
+
+Vue.use(require('@marklet/renderer'))
+
 module.exports = {
-  el: '#app'
+  el: '#app',
+
+  data: () => ({
+    source: '',
+    nodes: [],
+  }),
+
+  watch: {
+    source(value) {
+      this.nodes = this.lexer.parse(value)
+    },
+  },
+
+  created() {
+    this.lexer = new DocumentLexer()
+  },
 }
 
 </script>
 
 <template>
-  <div>{{ 1 }}</div>
+  <div>
+    <div class="input">
+      <textarea v-model="source"/>
+    </div>
+    <div class="output">
+      <ml-nodes :content="nodes"/>
+    </div>
+  </div>
 </template>
 
+<style lang="scss" scoped>
+
+> .input, > .output {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 50%;
+}
+
+> .input {
+  left: 0;
+
+  > textarea {
+    resize: none;
+    width: -webkit-fill-available;
+    height: -webkit-fill-available;
+  }
+}
+
+> .output { right: 0 }
+
+</style>
